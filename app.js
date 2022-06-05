8afd68ca6d6da18055a2dca81820cdc05353c730
PMKhai <minhkhai3012@gmail.com>
2022-06-05 18:54:42 +0700

    setup database

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+const cors = require('cors');
+
+const db = require('./models');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// db.sequelize.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ models/user.model.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, Sequelize) => {
+  const user = sequelize.define('user', {
+    name: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    active: {
+      type: Sequelize.BOOLEAN,
+    },
+  });
+  return user;
+};
